8b41878b69cfb9e2a14bf4621e4f05932b5be8f4
Mohamed Ihsan <mohamedihshan170@gmail.com>
2025-03-12 20:58:03 +0530

    App

--- App.tsx
+++ App.tsx
@@ -7,6 +7,10 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Footer from './Screens/Footer';
 import Checkout from './Screens/CheckoutCard';
+import Login from './components/log&reg/Login';
+import Registration from './components/log&reg/Registration';
+import Logout from './Screens/Logout';
+import ProductAddForm from './components/ProductAddForm/ProductAddForm';
 
 const Drawer = createDrawerNavigator();
 
@@ -14,14 +18,19 @@ const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator
+        initialRouteName="Login"
         drawerContent={(props) => <Sidebar {...props} />}
         screenOptions={{
-          header: (props) => <Header {...props} />, // Add custom header
+          header: (props) => <Header {...props} />,
         }}
       >
         <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="MyOrder" component={ProductAddForm} />
+        <Drawer.Screen name="Logout" component={Logout} />
         <Drawer.Screen name="AddToCard" component={AddToCard} />
         <Drawer.Screen name="Checkout" component={Checkout} />
+        <Drawer.Screen name="Login" component={Login} />
+        <Drawer.Screen name="Registration" component={Registration} />
       </Drawer.Navigator>
       <Footer />
     </NavigationContainer>
